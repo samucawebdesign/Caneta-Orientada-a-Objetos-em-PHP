4fb8437f0af019e90e77c2132c49475644e5b1d6
samucawebdesign <samuca@samuca.eti.br>
2017-07-06 09:46:27 -0300

    Exemplo 1

---
+++ Caneta.php
@@ -0,0 +1,53 @@
+﻿<?PHP
+# Classe Caneta
+class Caneta
+	{
+		public $modelo;
+		private $ponta;
+		
+		protected $carga;
+		protected $tampada;
+		
+		public function getModelo()
+			{
+				return $this->modelo;
+			}
+			
+		public function setModelo($m)
+			{
+				$this->modelo = $m;
+			}
+			
+		public function getPonta()
+			{
+				return $this->ponta;
+			}
+		
+		public function setPonta($p)
+			{
+				$this->ponta = $p;
+			}
+		
+		public function rabiscar()
+			{
+				if($this->tampada == true)
+					{
+						echo '<br>Erro! Tá tampado, não dá pra rabiscar<br>';
+					}
+				else
+					{
+						echo '<br>Estou rabiscando<br>';
+					}
+			}
+			
+		public function tampar()
+			{
+				$this->tampada = true;
+			}
+			
+		public function destampar()
+			{
+				$this->tampada = false;
+			}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+	<pre>
+	<?PHP
+	require_once('Caneta.php');
+	$c1 = new Caneta;
+	$c1->modelo = 'Bic Cristal';
+	$c1->cor = 'Azul';
+	#$c1->ponta = 0.5;
+	#$c1->carga = 99;
+	#$c1->tampada = true;
+	
+	print_r($c1);
+	$c1->rabiscar();
+	$c1->tampar();
+	print_r($c1);
+	?>
+    </pre>
+</body>
+</html>
