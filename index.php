c5c6381ecb51254ea97536564680ad76a111ac1b
Samuca Webdesign <samuca@samuca.eti.br>
2017-07-07 14:09:01 -0300

    show! =)

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 	$c1 = new Caneta;
 	$c1->modelo = 'Bic Cristal';
 	$c1->cor = 'Azul';
-	#$c1->ponta = 0.5;
-	#$c1->carga = 99;
-	#$c1->tampada = true;
+	$c1->ponta = 0.5;
+	$c1->carga = 99;
+	$c1->tampada = true;
 	
-	print_r($c1);
+	#print_r($c1);
 	$c1->rabiscar();
 	$c1->tampar();
 	$c1->rabiscar();
